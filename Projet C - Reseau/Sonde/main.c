683409ca4b03e0ede0172efa035f4ad9545dd113
Brice Gaulupeau <gaulupeau.brice@gmail.com>
2012-11-03 22:13:34 +0100

    Commit Docs

--- Projet C - Reseau/Sonde/main.c
+++ Projet C - Reseau/Sonde/main.c
@@ -6,27 +6,12 @@
 
 void * waitForClient(void * ctx)
 {
-    while(1)
-    {
-        printf("\t\t[LOOOL]\n");
-        fflush(stdout);
-    }
-
     return NULL;
 }
 
 int main()
 {
-    pthread_t threadWait;
-    int retThread = pthread_create(&threadWait, NULL, waitForClient, NULL);
 
-    if(retThread == 0)
-    {
-    	printf("\t\t[REUSSI]\n");
-    	/* On stoke l'identifiant du thread dans le tableau */
-    	//tThreadList[counterThreadClients] = &thread;
-    	//counterThreadClients++;
-    }
 
     system("PAUSE");
     return 0;
